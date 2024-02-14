aa500e57d8bdf51c90c20d3a6c815fdc76f716c3
Rolson Quadras <rolson.quadras@gendigital.com>
2024-02-14 11:03:54 -0500

    refactor: remove sidetreelongform vdr impl (#26)
    The sidetreelongform vdr impl was moved to sidetree-go repo (https://github.com/trustbloc/sidetree-go/pull/9) to remove did-go dependecy on sidetree-go repo.
    
    Signed-off-by: Rolson Quadras <rolson.quadras@gendigital.com>

---
+++ pkg/canonicalizer/canonicalizer.go
@@ -0,0 +1,29 @@
+/*
+Copyright Gen Digital Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package canonicalizer
+
+import (
+	"encoding/json"
+
+	"github.com/trustbloc/did-go/pkg/internal/jsoncanonicalizer"
+)
+
+// MarshalCanonical is using JCS RFC canonicalization.
+func MarshalCanonical(value interface{}) ([]byte, error) {
+	valueBytes, ok := value.([]byte)
+
+	if !ok {
+		var err error
+
+		valueBytes, err = json.Marshal(value)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return jsoncanonicalizer.Transform(valueBytes)
+}
