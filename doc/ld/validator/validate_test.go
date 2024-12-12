fd71069d9b4020fc0a1b3566e3ece5ea88fcbf8f
Stas Dm <iqpirat@gmail.com>
2024-12-12 15:50:09 +0100

    feat: update context

--- doc/ld/validator/validate_test.go
+++ doc/ld/validator/validate_test.go
@@ -638,6 +638,29 @@ func TestValidateType(t *testing.T) {
 		assert.NoError(t, ValidateJSONLDTypes(parsed, WithDocumentLoader(loader)))
 	})
 
+	t.Run("valid 2", func(t *testing.T) {
+		var parsed map[string]interface{}
+		assert.NoError(t, json.Unmarshal([]byte(`{
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://www.w3.org/2018/credentials/examples/v1"
+  ],
+  "id": "http://example.edu/credentials/58473",
+  "type": ["VerifiableCredential", "AlumniCredential"],
+  "issuer": "https://example.edu/issuers/14",
+  "issuanceDate": "2010-01-01T19:23:24Z",
+  "credentialSubject": {
+    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+    "alumniOf": "Example University"
+  },
+  "proof": {
+    "type": "RsaSignature2018"
+  }
+}`), &parsed))
+
+		assert.NoError(t, ValidateJSONLDTypes(parsed, WithDocumentLoader(loader)))
+	})
+
 	t.Run("invalid length", func(t *testing.T) {
 		assert.ErrorContains(t, validateTypesInExpandedDoc([]any{
 			1,
